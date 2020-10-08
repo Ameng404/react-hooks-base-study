747906dba945a1810cfb8cc882b39d522a9fda02
Ameng404 <ameng404@qq.com>
2020-10-08 14:27:59 +0800

    useContext demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, createContext, useContext } from 'react';
 import './App.css';
 
+const countContext = createContext(0)
+
+const Count: React.FC = () => {
+  const count = useContext(countContext)
+  return <div>{count}</div>
+}
+
 const App: React.FC = () => {
+  const [count, setCount] = useState(0)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <countContext.Provider value={count}>
+          <Count />
+        </countContext.Provider>
+        <button onClick={() => setCount(count + 1)}>+1</button>
       </header>
     </div>
   );
