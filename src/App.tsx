88d51859f5e0b05fdfbad4e66b1948f882857d4e
Ameng404 <ameng404@qq.com>
2020-10-26 23:13:15 +0800

    feat:调整代码

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,18 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+interface Hits {
+  author: string;
+  created_at: string;
+  created_at_i: number;
+  num_comments: number;
+  objectID: string;
+  points: number;
+  title: string;
+  url: string;
+}
+
+
 const App: React.FC = () => {
   const [data, setData] = useState({ hits: [] });
 
@@ -10,7 +22,6 @@ const App: React.FC = () => {
       const result = await axios(
         'https://hn.algolia.com/api/v1/search?query=redux',
       );
-  
       setData(result.data);
     };
 
@@ -19,7 +30,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <ul>
-        {data.hits.map(item => (
+        {data.hits.map((item: Hits) => (
           <li key={item.objectID}>
             <a href={item.url}>{item.title}</a>
           </li>
@@ -30,3 +41,5 @@ const App: React.FC = () => {
 }
 
 export default App;
+
+// 
